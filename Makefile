cb8e11038d84e89295e38faf225717e9fa819ce5
Jason Brown <jasonwbrown23@gmail.com>
2021-08-31 15:03:44 -0700

    Bumped coverage to 33.6

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 cover: 
 	go test -v -covermode=count -coverprofile=.cover ./...
+
+html:
+	go tool cover -html=.cover
--- svc/CoinbaseSvc_test.go
+++ svc/CoinbaseSvc_test.go
@@ -138,3 +138,81 @@ func TestCoinbaseSvc_GetMarketConditions(t *testing.T) {
 		})
 	}
 }
+
+func TestCoinbaseSvc_GetLastPrice(t *testing.T) {
+	type fields struct {
+		WantErr error
+		book    coinbasepro.Book
+	}
+	type args struct {
+		product string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    float64
+		wantErr error
+	}{
+		{
+			name: "Happy Path. Last Price Returns expected values.",
+			fields: fields{
+				WantErr: nil,
+				book: coinbasepro.Book{
+					Bids: []coinbasepro.BookEntry{
+						{
+							Price:          "44.0",
+							Size:           "1.0",
+							NumberOfOrders: 400,
+							OrderID:        "GUID-1",
+						},
+						{
+							Price:          "9.0",
+							Size:           "2.0",
+							NumberOfOrders: 800,
+							OrderID:        "GUID-2",
+						},
+					},
+				},
+			},
+			args: args{
+				"BTC-USD",
+			},
+			want:    44.0,
+			wantErr: nil,
+		},
+		{
+			name: "Sed Path. Client does not return error but there are no bids will return error.",
+			fields: fields{
+				WantErr: nil,
+				book: coinbasepro.Book{
+					Bids: []coinbasepro.BookEntry{},
+				},
+			},
+			args: args{
+				"BTC-USD",
+			},
+			want:    -100.0,
+			wantErr: fmt.Errorf("failed to get books expecting array to be populated"), //tight coupling between error messages is by design
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := proclient.NewMockClient()
+			c.Err = tt.fields.WantErr
+			c.Book = tt.fields.book
+			svc := CoinbaseSvc{
+				Client:  c,
+				Timeout: time.Duration(time.Millisecond), // for all tests, no backoff necessary
+			}
+			got, err := svc.GetLastPrice(tt.args.product)
+			if err != nil && err.Error() != tt.wantErr.Error() {
+				t.Errorf("CoinbaseSvc.GetLastPrice() err = %v, want %v", err, tt.wantErr)
+			}
+
+			if got != tt.want {
+				t.Errorf("CoinbaseSvc.GetLastPrice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
